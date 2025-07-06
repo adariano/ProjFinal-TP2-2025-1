b73a0af68653c7e6d813f85eff241dff93856c1e
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-07-06 12:42:52 -0300

    refactor: simplify user creation test by removing unnecessary server setup

--- supermercado-app/__tests__/api/users.test.ts
+++ supermercado-app/__tests__/api/users.test.ts
@@ -1,25 +1,10 @@
 // __tests__/users.test.ts
 import request from 'supertest';
-import { createServer } from 'http';
-// import { app } from 'next/app'; // Removido porque não é necessário
-import handler from "@/app/api/user/route"; // Ajuste o caminho conforme a estrutura do seu projeto
 
 describe('User API', () => {
   it('should create a user successfully', async () => {
-    const server = createServer(async (req, res) => {
-      if (req.method === 'POST' && req.url === '/api/users') {
-        const response = await handler(req as any);
-        res.statusCode = response.status!;
-        res.setHeader('Content-Type', 'application/json');
-        const body = await response.json();
-        res.end(JSON.stringify(body));
-      }
-    });
-
-    await new Promise<void>((resolve) => server.listen(4000, () => resolve()));
-
     const res = await request('http://localhost:3000')
-      .post('/api/users')
+      .post('/api/user')
       .send({
         name: 'João da Silva',
         email: 'joao@example.com',
@@ -29,7 +14,5 @@ describe('User API', () => {
     expect(res.statusCode).toBe(201);
     expect(res.body).toHaveProperty('id');
     expect(res.body.name).toBe('João da Silva');
-
-    server.close();
   });
 });
